35a4c9a2f953f8fa978fc7b5327d56ffe67bea7c
Anca Stanoev <150381628+ancastanoev@users.noreply.github.com>
2024-06-07 22:33:56 +0300

    Create Character.hpp

---
+++ Character.hpp
@@ -0,0 +1,19 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Character {
+public:
+    std::string name;
+    std::vector<std::string> responses;
+
+    Character(const std::string& name, const std::vector<std::string>& responses);
+
+    friend std::ostream& operator<<(std::ostream& os, const Character& character);
+    bool operator==(const Character& other) const;
+};
+
+#endif // CHARACTER_H
